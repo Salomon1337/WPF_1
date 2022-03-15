074b335feeb0d6a8469e5ec849f77c69487406fe
Salomon1337 <salomonpl2000@gmail.com>
2022-03-15 20:30:13 +0100

    Add project files.

---
+++ WPF_1/Lottery.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_1
+{
+    class Lottery
+    {
+        private Random random = new Random();
+        private List<string> list = new List<string>();
+
+        public Lottery()
+        {
+            list.Add("rower");
+            list.Add("samochod");
+            list.Add("dom");
+            list.Add("łopata");
+        }
+
+        public void Insert (String word)
+        {
+            list.Add(word);
+        }
+        public string TakeOutAndDelete()
+        {
+
+            int index = random.Next(0, list.Count);
+            string word = list[index];
+            list.RemoveAt(index);
+            return word;
+        }
+        public bool IsEmpty()
+        {
+            if (list.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ WPF_1/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private Lottery Lottery;
+        public MainWindow()
+        {
+            InitializeComponent();
+            Lottery = new Lottery();
+        }
+
+        private void Button_Click_Wrzuc(object sender, RoutedEventArgs e)
+        {
+            string word = Input.Text;
+            Lottery.Insert(word);
+            Input.Text = "Wpisz słowo";
+        }
+
+        private void Button_Click_Wyjmij(object sender, RoutedEventArgs e)
+        {
+            if (Lottery.IsEmpty() == false)
+            {
+                Label.Content = Lottery.TakeOutAndDelete();
+            }
+
+        }
+    }
+}
